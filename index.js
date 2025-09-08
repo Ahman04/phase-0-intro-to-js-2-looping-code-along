77a86a06bc2d49eaa85773ee28fae50cda971d61
Ahman04 <abokey2020@gmail.com>
2025-09-08 15:21:56 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// 1. writeCards
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+// 2. countDown
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+// export if needed
+module.exports = { writeCards, countDown };
